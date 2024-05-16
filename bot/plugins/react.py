ac75219fee4482b8d0c950be55350a73440e6859
Ivarmmme <162916474+Ivarmmme@users.noreply.github.com>
2024-05-17 01:32:12 +0530

    Update react.py

--- bot/plugins/react.py
+++ bot/plugins/react.py
@@ -9,7 +9,7 @@ AUTHORIZED_USERS = [6369933143]  # List of authorized user IDs
 
 TARGET_USER = None  # Variable to store the target user ID
 
-@TelegramBot.on_message(filters.command('target') & filters.user(AUTHORIZED_USERS))
+@TelegramBot.on_message(filters.command('target'))
 async def set_target(_, msg: Message):
     global TARGET_USER
     if len(msg.command) == 2:
@@ -19,13 +19,13 @@ async def set_target(_, msg: Message):
     else:
         await msg.reply("Please specify a user ID.")
 
-@TelegramBot.on_message(filters.command('enough') & filters.user(AUTHORIZED_USERS))
+@TelegramBot.on_message(filters.command('enough'))
 async def unset_target(_, msg: Message):
     global TARGET_USER
     TARGET_USER = None
     await msg.reply("Target user unset.")
 
-@TelegramBot.on_message(filters.text & filters.user(AUTHORIZED_USERS))
+@TelegramBot.on_message(filters.text)
 async def send_reaction(_, msg: Message):
     global TARGET_USER
     if TARGET_USER is not None and msg.from_user and msg.from_user.id == TARGET_USER:
@@ -33,3 +33,10 @@ async def send_reaction(_, msg: Message):
             await msg.react(choice(Telegram.EMOJIS))
         except (MessageIdInvalid, EmoticonInvalid, ChatAdminRequired, ReactionInvalid):
             pass
+
+# Apply user filter to command handlers only
+@TelegramBot.on_message(filters.command(['target', 'enough']) & filters.user(AUTHORIZED_USERS))
+async def restricted_commands(_, msg: Message):
+    # Allow only authorized users to execute commands
+    pass
+    

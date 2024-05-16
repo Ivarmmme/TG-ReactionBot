ce3374a178bff08122d3959d2f2dd0dded3f7447
Ivarmmme <162916474+Ivarmmme@users.noreply.github.com>
2024-05-17 01:53:33 +0530

    Update react.py

--- bot/plugins/react.py
+++ bot/plugins/react.py
@@ -5,30 +5,28 @@ from random import choice
 from bot import TelegramBot
 from bot.config import Telegram
 
-TARGET_USER = None  # Variable to store the target user ID
+ALLOWED_USER_ID = 123456789  # Replace with the ID of the user who should have access
 
 @TelegramBot.on_message(filters.command('target'))
 async def set_target(_, msg: Message):
     global TARGET_USER
-    replied_msg = msg.reply_to_message
-    if replied_msg and replied_msg.from_user:
-        target_id = replied_msg.from_user.id
-        TARGET_USER = target_id
-        await msg.reply(f"Target user set to {target_id}")
+    if msg.from_user and msg.from_user.id == ALLOWED_USER_ID:
+        replied_msg = msg.reply_to_message
+        if replied_msg and replied_msg.from_user:
+            target_id = replied_msg.from_user.id
+            TARGET_USER = target_id
+            await msg.reply(f"Target user set to {target_id}")
+        else:
+            await msg.reply("Please reply to a message from the target user to set the target.")
     else:
-        await msg.reply("Please reply to a message from the target user to set the target.")
+        await msg.reply("You are not authorized to use this command.")
 
 @TelegramBot.on_message(filters.command('enough'))
 async def unset_target(_, msg: Message):
     global TARGET_USER
-    TARGET_USER = None
-    await msg.reply("Target user unset.")
-
-@TelegramBot.on_message(filters.text)
-async def send_reaction(_, msg: Message):
-    global TARGET_USER
-    if TARGET_USER is not None and msg.from_user and msg.from_user.id == TARGET_USER:
-        try:
-            await msg.react(choice(Telegram.EMOJIS))
-        except (MessageIdInvalid, EmoticonInvalid, ChatAdminRequired, ReactionInvalid):
-            pass
+    if msg.from_user and msg.from_user.id == ALLOWED_USER_ID:
+        TARGET_USER = None
+        await msg.reply("Target user unset.")
+    else:
+        await msg.reply("You are not authorized to use this command.")
+        

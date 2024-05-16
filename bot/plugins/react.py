32c2766a0ea5d7f4d9b8f2979d6621d140126fd2
Ivarmmme <162916474+Ivarmmme@users.noreply.github.com>
2024-05-17 01:57:36 +0530

    Update react.py

--- bot/plugins/react.py
+++ bot/plugins/react.py
@@ -5,7 +5,8 @@ from random import choice
 from bot import TelegramBot
 from bot.config import Telegram
 
-ALLOWED_USER_ID = 6369933143  # Replace with the ID of the user who should have access
+ALLOWED_USER_ID = 123456789  # Replace with the ID of the user who should have access
+TARGET_USER = None  # Variable to store the target user ID
 
 @TelegramBot.on_message(filters.command('target'))
 async def set_target(_, msg: Message):
@@ -13,9 +14,8 @@ async def set_target(_, msg: Message):
     if msg.from_user and msg.from_user.id == ALLOWED_USER_ID:
         replied_msg = msg.reply_to_message
         if replied_msg and replied_msg.from_user:
-            target_id = replied_msg.from_user.id
-            TARGET_USER = target_id
-            await msg.reply(f"Target user set to {target_id}")
+            TARGET_USER = replied_msg.from_user.id
+            await msg.reply(f"Target user set to {TARGET_USER}")
         else:
             await msg.reply("Please reply to a message from the target user to set the target.")
     else:
@@ -29,4 +29,12 @@ async def unset_target(_, msg: Message):
         await msg.reply("Target user unset.")
     else:
         await msg.reply("You are not authorized to use this command.")
-        
+
+@TelegramBot.on_message(filters.text)
+async def send_reaction(_, msg: Message):
+    global TARGET_USER
+    if TARGET_USER is not None and msg.from_user and msg.from_user.id == TARGET_USER:
+        try:
+            await msg.react(choice(Telegram.EMOJIS))
+        except (MessageIdInvalid, EmoticonInvalid, ChatAdminRequired, ReactionInvalid):
+            pass

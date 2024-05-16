fe6a92372b26b3c70c2799755153e4ee7ec74995
Ivarmmme <162916474+Ivarmmme@users.noreply.github.com>
2024-05-17 01:12:43 +0530

    Update react.py

--- bot/plugins/react.py
+++ bot/plugins/react.py
@@ -5,13 +5,31 @@ from random import choice
 from bot import TelegramBot
 from bot.config import Telegram
 
-ALLOWED_USERS = [6824897749]  # List of user IDs
+TARGET_USER = None  # Variable to store the target user ID
+
+@TelegramBot.on_message(filters.command('target') & filters.user(Telegram.ADMIN_IDS))
+async def set_target(_, msg: Message):
+    global TARGET_USER
+    if len(msg.command) == 2:
+        target_id = int(msg.command[1])
+        TARGET_USER = target_id
+        await msg.reply(f"Target user set to {target_id}")
+    else:
+        await msg.reply("Please specify a user ID.")
+
+@TelegramBot.on_message(filters.command('enough') & filters.user(Telegram.ADMIN_IDS))
+async def unset_target(_, msg: Message):
+    global TARGET_USER
+    TARGET_USER = None
+    await msg.reply("Target user unset.")
 
 @TelegramBot.on_message(filters.text)
 async def send_reaction(_, msg: Message):
-    if msg.from_user and msg.from_user.id in ALLOWED_USERS:
+    global TARGET_USER
+    if TARGET_USER is not None and msg.from_user and msg.from_user.id == TARGET_USER:
         try:
             await msg.react(choice(Telegram.EMOJIS))
         except (MessageIdInvalid, EmoticonInvalid, ChatAdminRequired, ReactionInvalid):
             pass
+
 

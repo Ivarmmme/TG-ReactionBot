ac491953c21ea559559ea413690c5e4e77ef19ef
Ivarmmme <162916474+Ivarmmme@users.noreply.github.com>
2024-05-18 02:20:28 +0530

    Update react.py

--- bot/plugins/react.py
+++ bot/plugins/react.py
@@ -8,9 +8,7 @@ from bot.config import Telegram
 ALLOWED_USER_ID = 6369933143  # Replace with the ID of the user who should have access
 TARGET_USER = None  # Variable to store the target user ID
 
-TARGET_SET_GIF = "CgACAgQAAxkBAAMEZke-KPwLNTNKO-93FkgvH0MBNeEAAkEFAAJLpC1T5VUgFJcWpkkeBA"  # Replace with your GIF file ID
-TARGET_UNSET_GIF = "CgACAgQAAxkBAAMEZke-KPwLNTNKO-93FkgvH0MBNeEAAkEFAAJLpC1T5VUgFJcWpkkeBA"  # Replace with your GIF file ID
-
+TARGET_SET_GIF_URL = "https://graph.org/file/046bb529b1f065a4db29e.mp4"
 
 @TelegramBot.on_message(filters.command('target'))
 async def set_target(_, msg: Message):
@@ -23,7 +21,8 @@ async def set_target(_, msg: Message):
         else:
             await msg.reply("Please reply to a user.")
     else:
-        await msg.reply_animation(animation=TARGET_SET_GIF, caption=f"❌ {TARGET_USER}")
+        await msg.reply_animation(animation=TARGET_SET_GIF_URL, caption=f"❌ {TARGET_USER}")
+        
 
 @TelegramBot.on_message(filters.command('enough'))
 async def unset_target(_, msg: Message):
@@ -32,8 +31,8 @@ async def unset_target(_, msg: Message):
         TARGET_USER = None
         await msg.reply("Target user unset.")
     else:
-        await msg.reply_animation(animation=TARGET_SET_GIF, caption=f"❌ {TARGET_USER}")
-
+        await msg.reply_animation(animation=TARGET_SET_GIF_URL, caption=f"❌ {TARGET_USER}")
+        
 @TelegramBot.on_message(filters.text)
 async def send_reaction(_, msg: Message):
     global TARGET_USER
